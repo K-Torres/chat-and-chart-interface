a7c4109c3a1ae0978875f8dea215aa09f201aa58
Johan Torres <keiver.torres.ext@farmatodo.com>
2024-10-14 09:31:28 -0500

    Poppins font added

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { Box, Container } from "@mui/material";
 import React from "react";
 import Chart from "./components/chart/Chart";
 import Chat from "./components/chat/ChatContainer";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./lib/theme";
 
 const App: React.FC = () => {
   return (
     <Container>
       <Box display="flex" sx={{ width: "100%", height: "100vh" }}>
-        <MainProvider>
-          <Box flex={3}>
-            <Chart />
-          </Box>
-          <Chat />
-        </MainProvider>
+        <ThemeProvider theme={theme}>
+          <MainProvider>
+            <Box flex={3}>
+              <Chart />
+            </Box>
+            <Chat />
+          </MainProvider>
+        </ThemeProvider>
       </Box>
     </Container>
   );
